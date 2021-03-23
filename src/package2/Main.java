b882521240cbf7638cfe571ba794ca0f814c3dba
LRobert0 <luis.roberto.revollo.montero@gmail.com>
2021-03-22 20:23:11 -0400

    bucles de codigo a funciones reduciendo a 1/3 del largo

--- src/package2/Main.java
+++ src/package2/Main.java
@@ -3,135 +3,76 @@ package package2;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String [] args){
-		/*
-		int notas [] = {5,5,8,6,3,4};
-		int respuesta =0;
+	public static int suma(int numero1, int numero2){
+		return numero1+numero2;
+	}
+	public static int resta(int numero1, int numero2){
+		return numero1-numero2;
+	}
+	public static int multiplicacion(int numero1, int numero2){
+		return numero1*numero2;
+	}
+	public static int division(int numero1, int numero2){
+		return numero1/numero2;
+	}
 		
-		for (int i=0; i< notas.length; i++) {
-			respuesta = respuesta+notas[i];
-			
-			
-		}
-		float respuestaf = (float)respuesta/6;
-		System.out.println(respuestaf);
-		*/
+	public static void main(String [] args){
+		
+		int respuesta =0;
+		Scanner inscan = new Scanner(System.in);
 		int condition = 1;
 		System.out.println("Bienvenido a mini calculadora");
 		while(condition ==1) {
+			
 			System.out.println("Por favor introduce que operacion harás,");
 			System.out.println("Suma(1), Resta(2), Multiplicación(3), Division(4)");
-			Scanner inscan = new Scanner(System.in);
-			var operacion = inscan.nextLine();
-			int operacionint = Integer.parseInt(operacion);
-			if (operacionint==1) {
+			int operacionint = Integer.parseInt(inscan.nextLine());
+			if (operacionint < 5) {
+				
 				System.out.println("Introduce tu primer numero:");
-				var numero1 = inscan.nextLine();
-				int numero1int = Integer.parseInt(numero1);
+				int numero1int = Integer.parseInt(inscan.nextLine());
 				System.out.println("Introduce tu segundo numero:");
-				var numero2 = inscan.nextLine();
-				int numero2int = Integer.parseInt(numero2);
-				int respuesta = numero1int+numero2int;
-				System.out.println(numero1+" + "+numero2+" = "+respuesta);
+				int numero2int = Integer.parseInt(inscan.nextLine());
+				System.out.println(operacionint);
+				switch(operacionint){
+				
+					case 1 :
+						respuesta = suma(numero1int,numero2int);
+						System.out.println(numero1int+" + "+numero2int+" = "+respuesta);
+						break;
+					
+					case 2 :
+						respuesta = resta(numero1int,numero2int);
+						System.out.println(numero1int+" - "+numero2int+" = "+respuesta);
+						break;
+					
+					case 3 :
+						respuesta = multiplicacion(numero1int,numero2int);	
+						System.out.println(numero1int+" x "+numero2int+" = "+respuesta);
+						break;
+					
+					case 4 :
+						respuesta = division(numero1int,numero2int);
+						System.out.println(numero1int+" ÷ "+numero2int+" = "+respuesta);
+						break;
+												
+				}
 				System.out.println("Quieres continuar? Si(1) No(2)");
-				var continuar = inscan.nextLine();
-				int continuarint = Integer.parseInt(continuar);
-				if (continuarint==1)
-				{
+				int continuarint = Integer.parseInt(inscan.nextLine());
+				if (continuarint==1){
+					
 					System.out.println("continuemos...");
 					condition =1;
 				}
-				else
-				{
-					System.out.println("adiós!");
+				else{
+					
+					System.out.println("Adiós!");
 					condition=0;
 				}
 			}
-			else
-			{
-				if (operacionint==2) {
-					System.out.println("Introduce tu primer numero:");
-					var numero1 = inscan.nextLine();
-					int numero1int = Integer.parseInt(numero1);
-					System.out.println("Introduce tu segundo numero:");
-					var numero2 = inscan.nextLine();
-					int numero2int = Integer.parseInt(numero2);
-					int respuesta = numero1int-numero2int;
-					System.out.println(numero1+" - "+numero2+" = "+respuesta);
-					System.out.println("Quieres continuar? Si(1) No(2)");
-					var continuar = inscan.nextLine();
-					int continuarint = Integer.parseInt(continuar);
-					if (continuarint==1)
-					{
-						System.out.println("continuemos...");
-						condition =1;
-					}
-					else
-					{
-						System.out.println("adiós!");
-						condition=0;
-					}
-				}
-				else
-				{
-					if (operacionint==3) {
-						System.out.println("Introduce tu primer numero:");
-						var numero1 = inscan.nextLine();
-						int numero1int = Integer.parseInt(numero1);
-						System.out.println("Introduce tu segundo numero:");
-						var numero2 = inscan.nextLine();
-						int numero2int = Integer.parseInt(numero2);
-						int respuesta = numero1int*numero2int;
-						System.out.println(numero1+" x "+numero2+" = "+respuesta);
-						System.out.println("Quieres continuar? Si(1) No(2)");
-						var continuar = inscan.nextLine();
-						int continuarint = Integer.parseInt(continuar);
-						if (continuarint==1)
-						{
-							System.out.println("continuemos...");
-							condition =1;
-						}
-						else
-						{
-							System.out.println("adiós!");
-							condition=0;
-						}
-					}
-					else
-					{
-						if (operacionint==4) {
-							System.out.println("Introduce tu primer numero:");
-							var numero1 = inscan.nextLine();
-							int numero1int = Integer.parseInt(numero1);
-							System.out.println("Introduce tu segundo numero:");
-							var numero2 = inscan.nextLine();
-							float numero1f=Float.parseFloat(numero1);  
-							float numero2f=Float.parseFloat(numero2);  
-							float respuesta = numero1f/numero2f;
-							System.out.println(numero1+" ÷ "+numero2+" = "+respuesta);
-							System.out.println("Quieres continuar? Si(1) No(2)");
-							var continuar = inscan.nextLine();
-							int continuarint = Integer.parseInt(continuar);
-							if (continuarint==1)
-							{
-								System.out.println("continuemos...");
-								condition =1;
-							}
-							else
-							{
-								System.out.println("adiós!");
-								condition=0;
-							}
-						}
-						else
-						{
-							System.out.println("Operacion no valida,");
-						}
-					}
-				}
+			else {
+				System.out.println("Operación no válida,");
 			}
 		}
-		
 	}
 }
